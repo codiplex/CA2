eb043c1f28103aea3bf997d090cb3ba02a99bef7
NaikUrRehman <paknaik.101@gmail.com>
2024-02-13 11:42:44 +0500

    updated task.py added index.html

--- task.py
+++ task.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    # Render the index.html template, which extends base.html
+    return render_template('index.html')
 
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
